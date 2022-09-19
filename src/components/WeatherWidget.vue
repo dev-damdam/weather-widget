9bc1b2f0e40d2e82e6272c67373c13658b2fad7e
dev-damdam <dev.sodam@gmail.com>
2022-09-20 01:07:51 +0900

    add WeatherView, WeatherWidget

---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="weather-widget-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: "WeatherWidget",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.weather-widget-wrapper {
+  width: 100%;
+}
+</style>
---
+++ src/views/WeatherView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="weather-view-wrapper">
+    <weather-widget />
+  </div>
+</template>
+
+<script>
+import WeatherWidget from "@/components/WeatherWidget.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    WeatherWidget,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.weather-view-wrapper {
+  width: 100%;
+}
+</style>
