eb3ee0d70377578b7d11eac30ca2751c99c2f220
dev-damdam <dev.sodam@gmail.com>
2022-09-19 17:07:02 +0900

    apply bootstrap on project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
 Vue.config.productionTip = false;
 
 new Vue({
